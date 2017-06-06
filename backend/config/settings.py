168e0ace1a610194f1109228b1b7035b238e8c55
dokenzy <dokenzy@gmail.com>
2017-06-06 21:22:25 +0900

    fixed static files directory path

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../../static'),
+    os.path.join(os.path.dirname(BASE_DIR), 'static'),
 ]
 
 WEBPACK_LOADER = {
