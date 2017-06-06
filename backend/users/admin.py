6c69471cd96290e9b1f69133a55b92aa935884b7
dokenzy <dokenzy@gmail.com>
2017-06-06 21:14:28 +0900

    - added twitter, facebook field in User model - added social account field in User Admin form

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+from django.utils.translation import ugettext_lazy as _
 
-from .models import User
+User = get_user_model()
 
 
-admin.site.register(User, UserAdmin)
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+        (_('Social'), {'fields': ('github_id', 'twitter_id', 'facebook_id')}),
+    )
+
+admin.site.register(User, MyUserAdmin)
--- backend/users/models.py
+++ backend/users/models.py
@@ -5,3 +5,5 @@ from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
     github_id = models.CharField(_('Github ID'), max_length=50, null=True, blank=True)
+    twitter_id = models.CharField(_('Twitter ID'), max_length=50, null=True, blank=True)
+    facebook_id = models.CharField(_('Facebook ID'), max_length=50, null=True, blank=True)
