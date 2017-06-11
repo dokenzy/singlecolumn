c44e2f4ea9e546b5b56567a96dc2b8e90add559b
dokenzy <dokenzy@gmail.com>
2017-06-11 22:51:24 +0900

    # 필요없는 설정 제거

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,6 @@ module.exports = {
                         // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
                         // the "scss" and "sass" values for the lang attribute to the right configs here.
                         // other preprocessors should work out of the box, no loader config like this nessessary.
-                        'css': 'vue-style-loader!css-loader',
                         'scss': 'vue-style-loader!css-loader!sass-loader',
                         'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
                     }
@@ -55,13 +54,6 @@ module.exports = {
                 test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
                 loader: 'file-loader'
             },
-          {
-             test: /\.css$/,
-             use: [
-                  'style-loader',
-                  { loader: 'css-loader', options: { importLoaders: 1 } }
-             ]
-          },
           {
               test: /\.styl$/,
               use: [
